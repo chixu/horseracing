0e26dccc1a43348c666c1d84ab22da693d22efad
Chi Xu <CX@CC.local>
2019-04-19 16:51:46 +0800

    gulp serve listen to js file only (resolve log issue)

--- gulp/serve.js
+++ gulp/serve.js
@@ -6,14 +6,15 @@ gulp.task("serve", function () {
   let port = argv.p || 3000;
   browserSync.init({
     port: port,
+    files: ["./js/*.js"],
     server: {
       baseDir: "./"
     }
   });
 
   gulp.watch([
-    "**/index.html",
-    "**/js/*.js"
+    // "**/index.html",
+    "./js/*.js"
   ]).on("change", () => browserSync.reload());
 
 });
